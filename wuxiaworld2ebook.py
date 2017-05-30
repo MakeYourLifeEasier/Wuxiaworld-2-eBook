4d38e7b45b61c3df31984182b8530e12fc754b93
MrHaCkEr <michiwaechter@googlemail.com>
2017-05-30 21:14:25 +0200

    Added Complete Novels

--- wuxiaworld2ebook.py
+++ wuxiaworld2ebook.py
@@ -7,10 +7,7 @@ print (links.index())
 novel = input("Which Novel do you want to read? (Enter a number): ")
 novel = int(novel)
 book = 0
-if novel == 43:
-	novel = input("Which Novel do you want to read? (Enter a number): ")
-if novel == 11 or novel == 18 or novel == 25 or novel == 40 or novel == 45 or novel == 46 or novel == 50:
-	links.IndexComplete()
+if novel == 11 or novel == 18 or novel == 25 or novel == 40 or novel == 44 or novel == 45 or novel == 49:
 	book = input("Which Book do you want to read?")
 raw_info = links.info(novel, book)
 s_chapter = input("What chapter do you want to start at?: ")
